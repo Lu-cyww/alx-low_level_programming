900c9727da71f412f68a540fe0c27dad480effb9
Lu-cyww <lucywangari659@gmail.com>
2022-09-11 22:06:24 -0700

    The last line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,9 @@ int main(void)
 {
 	int n, lastDigit;
 
-	lastDigit = n % 10
+	lastDigit = n % 10;
+
+	printf(" \n The Last Digit of n %d =  %d", n, lastDigit);
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
