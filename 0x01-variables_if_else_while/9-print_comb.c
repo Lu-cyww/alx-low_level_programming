3a310507f2d61f5dc50a3d57127767d3a88ba714
Lu-cyww <lucywangari659@gmail.com>
2022-09-12 14:04:30 -0700

    combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,21 +10,13 @@
 
 int main(void)
 {
-	int num1, num2;
+	int num1;
 
 	for (num1 = 0; num1 < 10; num1++)
-	{
-		for (num2 = 0; num2 < 10; num2++)
-		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 9 && num2 == 9)
-				continue;
+		putchar(num1)
 
 			putchar(',');
 			putchar(' ');
-		}
 	}
 
 	return (0);
