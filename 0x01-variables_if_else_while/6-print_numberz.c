e4dc1b5e733a3b61d972cb83cbab61020350c6d5
Lu-cyww <lucywangari659@gmail.com>
2022-09-12 12:20:25 -0700

    6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this is my main function
+ * description: Writes numbers with base 10 starting from 0
+ * Return: 0 always SUCCESS
+ */
+
+int main(void)
+{
+	putchar N;
+
+	for (N = '0'; N < '10'; N++)
+		putchar((N % 10) + '0');
+
+	putchar ('\n')
+
+	return (0);
+}
