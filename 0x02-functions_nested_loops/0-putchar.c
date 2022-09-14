d5e95fa7b74166f19c73af0a1eaf37201e58b1a4
Lu-cyww <lucywangari659@gmail.com>
2022-09-13 20:49:06 -0700

    My header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * main - this is my main function
+ * description: prints -putchar
+ * return: 0 when success
+ */
+
+int main(void);
+{
+	printf("-putchar\n")
+
+	return (0);
+}
