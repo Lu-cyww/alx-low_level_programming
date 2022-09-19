3a967b115261c5160897f22e13ed4517786737a9
Lu-cyww <lucywangari659@gmail.com>
2022-09-19 03:36:08 -0700

    peints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string
+ *
+ * Description: prints a string
+ * Return: no return value
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar (*(str + i));
+		i++;
+
+	}
+	putchar(10);
+}
