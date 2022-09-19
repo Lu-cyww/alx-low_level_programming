6fe48c4630a95cf4ea63cdfc2ebd826d17e9f79d
Lu-cyww <lucywangari659@gmail.com>
2022-09-19 02:52:15 -0700

    Reseting a value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,23 +2,11 @@
 
 /**
  * reset_to_98 - is my function
- *
+ * @n: parameter
  * Return: 0 when success
  */
 
 void reset_to_98(int *n)
 {
-	int n = 402;
-	int *ptr = &n;
-
-	_putchar("%d\n", n);
-	_putchar("%d\n", *ptr);
-
-	*ptr = 98;
-
-	_putchar("%d\n", n);
-	_putchar("%d\n", *pts);
-
-	return (0);
+	*n = 98;
 }
- 
