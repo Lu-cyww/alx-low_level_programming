c255bae2da306ddb5ac9dd4468974114222ec810
Lu-cyww <lucywangari659@gmail.com>
2022-09-21 20:57:58 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is my main function
+ *
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
