8262e62622714a324f224bfcc82df929220cd65a
Lu-cyww <lucywangari659@gmail.com>
2022-09-22 00:03:04 -0700

    checks if character is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - my function
+ * description: checks if character is uppercase
+ * @c: checks the the input of a function
+ * Return: 1 if uppercase and 0 when success
+ */
+
+int_isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - a function that writes the character c to stdout
+ * @c: my character
+ *
+ * Return: 1 on success
+ * On error. -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1. &c. 1));
+}
