521eb6d31fa7346441049fcf928b8bcb99ef2868
Lu-cyww <lucywangari659@gmail.com>
2022-09-30 13:43:05 -0700

    Argc Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - my main function
+ * @argc: counts the no. of arguments
+ * @argv: Arguments
+ *
+ * Return: 0 when success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
