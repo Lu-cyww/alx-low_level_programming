01096de6e00cbaf4ddf9a3603d318514c7f24202
Lu-cyww <lucywangari659@gmail.com>
2022-10-04 13:44:16 -0700

    Malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - string duplicator
+ * @str: String
+ * Return: String ponter
+ */
+
+char *_strdup(char *str)
+{
+	int j = 1, k = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[j])
+		j++;
+	s = (char *)malloc(i * sizeof(char) + 1);
+
+	if (s == NULL)
+		return (NULL);
+	while (k < j)
+	{
+		s[k] = str[k];
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
+}
